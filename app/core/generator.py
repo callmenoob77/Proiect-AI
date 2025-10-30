dbea910be02f6d76e609132a4894114b4891c114
rebecca-sacarescu <rebeccasacarescu@yahoo.com>
2025-10-30 14:59:30 +0200

    added problems

--- app/core/generator.py
+++ app/core/generator.py
@@ -2,35 +2,100 @@ import random
 import json
 
 PROBLEM_KNOWLEDGE = [
+
     {
-        "problem_name": "n-queens",
-        "correct_strategy": "Backtracking",
-        "example_instances": ["o tablă de 4x4", "găsirea unei soluții pe o tablă de 8x8", "o tablă de 6x6"],
-        "chapter_name": "Algoritmi de căutare și CSP",
-        "enum_type": "N_QUEENS"
-    },
-    {
-        "problem_name": "generalised Hanoi",
-        "correct_strategy": "Divide et Impera",
-        "example_instances": ["3 discuri și 4 tije", "5 discuri și 5 tije", "4 discuri și 4 tije"],
+        "problem_name": "Generalized Hanoi Towers",
+        "correct_strategy": "DFS (Depth-First Search)",
+        "example_instances": [
+            "3 discuri si 4 tije",
+            "5 discuri si 5 tije",
+            "4 discuri si 4 tije"
+        ],
         "chapter_name": "Strategii algoritmice",
         "enum_type": "HANOI"
     },
+
     {
-        "problem_name": "graph coloring",
-        "correct_strategy": "CSP (Constraint Satisfaction)",
-        "example_instances": ["colorarea hărții Australiei cu 3 culori", "colorarea unui graf cu 5 noduri și 4 muchii"],
+        "problem_name": "Sliding puzzle problem",
+        "correct_strategy": "A* Search",
+        "example_instances": [
+            "puzzle 3x3 cu o piesa lipsa",
+            "puzzle 4x4 pornind de la o stare amestecata"
+        ],
+        "chapter_name": "Algoritmi de căutare și CSP",
+        "enum_type": "SLIDING_PUZZLE"
+    },
+
+    {
+        "problem_name": "X&O",
+        "correct_strategy": "Backtracking (DFS in spatiul starilor)",
+        "example_instances": [
+            "tabla 3x3 clasica",
+            "varianta 4x4 pentru un AI mai complex"
+        ],
+        "chapter_name": "Algoritmi de căutare și CSP",
+        "enum_type": "X_O"
+    },
+
+    {
+        "problem_name": "River crossing problem",
+        "correct_strategy": "BFS (Breadth-First Search)",
+        "example_instances": [
+            "problema taranului, lupului, caprei si verzei",
+            "transportarea a 3 persoane peste un rau cu o barca de 2 locuri"
+        ],
+        "chapter_name": "Algoritmi de căutare și CSP",
+        "enum_type": "RIVER_CROSSING"
+    },
+
+    {
+        "problem_name": "Water jug problem",
+        "correct_strategy": "Uniform Cost Search (sau BFS)",
+        "example_instances": [
+            "doua vase de 3L si 5L pentru obtinerea a 4L de apa",
+            "doua vase de 4L si 9L pentru obtinerea a 6L"
+        ],
+        "chapter_name": "Algoritmi de căutare și CSP",
+        "enum_type": "WATER_JUG"
+    },
+
+    {
+        "problem_name": "n-queens",
+        "correct_strategy": "Backtracking",
+        "example_instances": [
+            "tabla de 4x4",
+            "tabla de 8x8",
+            "tabla de 6x6"
+        ],
+        "chapter_name": "Algoritmi de căutare și CSP",
+        "enum_type": "N_QUEENS"
+    },
+
+    {
+        "problem_name": "Graph coloring",
+        "correct_strategy": "Backtracking (CSP)",
+        "example_instances": [
+            "colorarea hartii Australiei cu 3 culori",
+            "colorarea unui graf cu 5 noduri si 4 muchii"
+        ],
         "chapter_name": "Algoritmi de căutare și CSP",
         "enum_type": "GRAPH_COLORING"
     },
+
     {
-        "problem_name": "knight's tour",
-        "correct_strategy": "Backtracking cu euristica Warnsdorff",
-        "example_instances": ["o tablă de 5x5 pornind din colț", "găsirea unui tur complet pe o tablă de 6x6"],
+        "problem_name": "Knight's tour",
+        "correct_strategy": "DFS sau Hillclimbing",
+        "example_instances": [
+            "tabla de 5x5 pornind din colt",
+            "tur complet pe o tabla de 6x6"
+        ],
         "chapter_name": "Algoritmi de căutare și CSP",
         "enum_type": "KNIGHT_TOUR"
     }
+
 ]
+
+
 
 MASTER_STRATEGIES = [
     "Backtracking", "Divide et Impera", "CSP (Constraint Satisfaction)",
