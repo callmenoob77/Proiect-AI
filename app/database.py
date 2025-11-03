a90153eedec785ec4b81ea01e5a2df0e4aeec2d1
alexandraterchea <alexandra.terchea@yahoo.com>
2025-11-03 23:45:41 +0200

    reparat front, mutat proiect+bd

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:student@localhost/AI"
+DATABASE_URL = "postgresql://neondb_owner:npg_a83OkYANZHPe@ep-purple-scene-ag1f4jb0-pooler.c-2.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
